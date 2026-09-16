44f53019bdb8959114f3a9e7b4fc24fb4243a85d
Schepie <geert.schepers@gmail.com>
2026-09-16 23:23:08 +0200

    Light color added

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Padel Scoreboard PWA Service Worker
-const APP_VERSION = 'v1.4.6';
+const APP_VERSION = 'v1.4.7';
 const CACHE_NAME = `padel-scoreboard-${APP_VERSION}`;
 
 const PRECACHE_ASSETS = [
